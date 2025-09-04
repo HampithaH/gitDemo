b6f078eee940eedc01800db22cdce58682048edb
Hampitha H <hampithah@gmail.com>
2025-09-04 11:56:29 +0530

    gitdemo commit

---
+++ gitdemo/src/gitdemo/Welcome.java
@@ -0,0 +1,11 @@
+package gitdemo;
+
+public class Welcome {
+	
+	public static void main (String[] agrs) {
+		
+		System.out.println("Welcome to Git");
+		System.out.println("Pushing the code to Git");
+	}
+
+}
