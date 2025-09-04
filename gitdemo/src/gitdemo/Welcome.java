03f98d7781e4a7b60ef1f5e9984525b37c15426b
Hampitha H <hampithah@gmail.com>
2025-09-04 12:28:21 +0530

    welcome commit 2

--- gitdemo/src/gitdemo/Welcome.java
+++ gitdemo/src/gitdemo/Welcome.java
@@ -7,6 +7,7 @@ public class Welcome {
 		System.out.println("Welcome to Git");
 		System.out.println("Pushing the code to Git");
 		System.out.println("Pulling the code to Git");
+		System.out.println("Pushing the code to Remote Repo: attempt2");
 	}
 
 }
